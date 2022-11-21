b3562f9f02d0b5a6689ce3fb1f7da15230a91840
QA36 <QA36@DESKTOP-AESHFBQ>
2022-11-21 21:01:03 +0100

    init

---
+++ OrangeProject/OrangeProject/Driver/WebDrivers.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeProject.Driver
+{
+    public class WebDrivers
+    {
+        public static IWebDriver? Instance { get; set; }
+
+
+        public static void Initialize()
+        {
+            Instance = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            Instance.Manage().Window.Maximize();
+            Instance.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            Instance.Navigate().GoToUrl("https://opensource-demo.orangehrmlive.com/");
+        }
+
+
+        public static void CleanUp()
+        {
+            Instance?.Quit();
+        }
+
+    }
+}
---
+++ OrangeProject/OrangeProject/Page/DashboardPage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeProject.Page
+{
+    public class DashboardPage
+    {
+        private IWebDriver driver = WebDrivers.Instance;
+
+        public IWebElement UserMenu => driver.FindElement(By.ClassName("oxd-userdropdown-tab"));
+
+        public void SelectOption()
+        {
+            SelectElement element = new SelectElement
+        }
+    }
+}
